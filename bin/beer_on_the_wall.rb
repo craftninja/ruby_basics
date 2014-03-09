834f4dccb3266c0a945d9cf30df801200e4aeff9
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-08 21:10:14 -0700

    If user just pushes enter, it will exit the program

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,5 +1,11 @@
-bottles = gets.chomp.to_i
+bottles = gets.chomp
 
+while bottles == ""
+  puts "I need to know how many bottles to sing!"
+  bottles = -1
+end
+
+bottles = bottles.to_i
 
 while bottles >2
   song = "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\n"
@@ -7,9 +13,17 @@ while bottles >2
   puts song
   bottles -=1
 end
+
 if bottles == 2
   puts "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+  bottles -=1
 end
 
-puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
-puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+if bottles == 1
+  puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+  bottles -=1
+end
+
+if bottles == 0
+  puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+end
